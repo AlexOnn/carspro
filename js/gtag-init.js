6d0876113e40a73f9abcbe916057b3cfddd5a498
AlexOnn <45159056+AlexOnn@users.noreply.github.com>
2025-09-19 17:34:08 +0200

    Update gtag-init.js

--- js/gtag-init.js
+++ js/gtag-init.js
@@ -1,14 +1,17 @@
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-2P7FWXC988');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-2P7FWXC988');
 
 // Tracking button "Request Quote" and "Gauti pasiūlymą"
-document.querySelectorAll('a[href="#shipping-form"]').forEach(el => {
-  el.addEventListener('click', () => {
-    gtag('event', 'request_quote_click', {
-      'event_category': 'engagement',
-      'event_label': el.textContent.trim() // сохранит текст кнопки (EN или LT)
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('a[href="#shipping-form"]').forEach(el => {
+    el.addEventListener('click', () => {
+      console.log('GA4 event sent:', el.textContent.trim());
+      gtag('event', 'request_quote_click', {
+        'event_category': 'engagement',
+        'event_label': el.textContent.trim()
+      });
     });
   });
 });
