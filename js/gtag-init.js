801f64745a25c1fc61769f1bce1568c3ffad5048
AlexOnn <45159056+AlexOnn@users.noreply.github.com>
2025-09-19 16:52:20 +0200

    Update gtag-init.js

--- js/gtag-init.js
+++ js/gtag-init.js
@@ -3,3 +3,13 @@
   gtag('js', new Date());
   gtag('config', 'G-2P7FWXC988');
 
+// Tracking button "Request Quote" and "Gauti pasiūlymą"
+document.querySelectorAll('a[href="#shipping-form"]').forEach(el => {
+  el.addEventListener('click', () => {
+    gtag('event', 'request_quote_click', {
+      'event_category': 'engagement',
+      'event_label': el.textContent.trim() // сохранит текст кнопки (EN или LT)
+    });
+  });
+});
+
